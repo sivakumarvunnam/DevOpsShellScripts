fcac987bd0907e877748da2f5f09e10dc179c1dc
Sivakumar Vunnam <Sivakumar.Vunnam@bhnetwork.com>
2020-04-12 01:06:05 +0000

    fixed permissions and added new script to install maven

---
+++ Java.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+set -eu
+
+function installJava() {
+    if ! [ -x "$(command -v java)" ]; then
+    echo "Instaling Java..."
+    sudo add-apt-repository ppa:webupd8team/java
+    sudo apt-get update
+    sudo apt-get install -y openjdk-8-jre-headless
+    JAVA_HOME=`readlink -e $(which java)`
+    export PATH=$PATH:$JAVA_HOME
+    echo "Java home : ${JAVA_HOME}"
+  fi
+}
+
+installJava
---
+++ install_maven.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+# script to install maven
+mvn_version=${mvn_version:-3.6.3}
+url="http://www.mirrorservice.org/sites/ftp.apache.org/maven/maven-3/${mvn_version}/binaries/apache-maven-${mvn_version}-bin.tar.gz"
+install_dir="/opt/maven"
+
+if [ -d ${install_dir} ]; then
+    mv ${install_dir} ${install_dir}.$(date +"%Y%m%d")
+fi
+
+mkdir ${install_dir}
+echo 'Installing maven...'
+curl -fsSL ${url} | tar zx --strip-components=1 -C ${install_dir}
+
+export MAVEN_HOME=${install_dir}
+export M2_HOME=${install_dir}
+export M2=${install_dir}/bin
+export PATH=${install_dir}/bin:$PATH
+
+echo maven installed to ${install_dir}
+
+mvn --version
+
